1f6096ace574ff8b0ca06113329a85073e0f5be2
Thiago <aizen.maia@gmail.com>
2023-04-01 21:33:12 -0300

    criação do arquivo style.d.ts para tipagem do theme default

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    background-color: ${props => props.theme.primary}
+    background-color: ${props => props.theme.secondary}
 
     /* ${props => {
         return css `
